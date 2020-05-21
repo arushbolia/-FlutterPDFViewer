d1a0b25cb34c46ce1ccdf2476c144970c4634c5e
Arush Bolia <boliaarush@gmail.com>
2020-05-21 15:57:13 +0530

    opens pdf via file in storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,58 +10,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter PDF',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
 
-  void _incrementCounter() {
+  String pdfUrl = "http://www.pdf995.com/samples/pdf.pdf";
+  String pdfAsset = "assets/sample.pdf";
+  PDFDocument _doc;
+  bool _loading;
+
+  @override
+  void initState() {
+    super.initState();
+    _initPDF();
+  }
+
+  _initPDF() async {
     setState(() {
-      _counter++;
+      _loading = true;
+    });
+    final doc = await PDFDocument.fromAsset(pdfAsset);
+    setState(() {
+      _doc = doc;
+      _loading = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      appBar: AppBar( title: Text("Flutter PDF Demo"),),
+      body: _loading ? Center(child: CircularProgressIndicator(),) : PDFViewer(document: _doc, showPicker: false, ),
     );
   }
 }
